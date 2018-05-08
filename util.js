ebee9dc36ead6ac58418aefeb744ae04a3445349
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-05-08 17:55:17 +0100

    Update util.js

--- util.js
+++ util.js
@@ -2,7 +2,15 @@ class Util {
   constructor(emitter) {
     this.emitter = emitter;
   }
-  
+  resolveSymbol(sym) {
+    const symbol = Symbol.for(Symbol.keyFor(sym));
+    if (!Symbol.keyFor(symbol)) return sym;
+    return symbol.valueOf();
+  }
+  isString(val) {
+    if (val instanceof String || typeof val === "string") return true;
+    return false;
+  }
 }
 
 module.exports = Util;
