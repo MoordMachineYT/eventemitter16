884986ff15b240e09a0ce2d9ba1efa5b916bba33
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-05-08 23:58:56 +0100

    Update util.js

--- util.js
+++ util.js
@@ -1,15 +1,23 @@
 class Util {
   constructor(emitter) {
     this.emitter = emitter;
+    this.strings = {};
+    this.symbols = {};
   }
   resolveSymbol(sym) {
     const symbol = Symbol.for(Symbol.keyFor(sym));
     if (!Symbol.keyFor(symbol)) return sym;
-    return `AGXS37#EXE${Symbol.keyFor(symbol)}QSD`;
+    this.strings[symbol] = `AGXS37#EXE${Symbol.keyFor(symbol)}QSD`;
+    this.symbols[this.strings[symbol]] = symbol;
+    return this.strings[symbol];
   }
   isValid(val) {
     if (val instanceof String || typeof val === "string" || typeof val === "symbol") return true;
     return false;
+  }
+  symbolify(sym) {
+    if (!this.symbols[sym]) return sym;
+    return this.symbols[sym];
   }
 }
 
