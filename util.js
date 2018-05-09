eb589f6ce11c90602a7601e95a45e2f21c61a9f0
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-05-09 10:15:07 +0100

    Update util.js

--- util.js
+++ util.js
@@ -1,3 +1,22 @@
+function longestSeqContainedIn(a, b) { // Thanks Node!
+  for (var len = a.length; len >= 3; --len) {
+    for (var i = 0; i < a.length - len; ++i) {
+      for (var j = 0; j < b.length - len; ++j) {
+        let matches = true;
+        for (var k = 0; k < len; ++k) {
+          if (a[i + k] !== b[j + k]) {
+            matches = false;
+            break;
+          }
+        }
+        if (matches)
+          return [ len, i, j ];
+      }
+    }
+  }
+  return [ 0, 0, 0 ];
+}
+
 class Util {
   constructor(emitter) {
     this.emitter = emitter;
@@ -19,33 +38,16 @@ class Util {
     if (!this.symbols[sym]) return sym;
     return this.symbols[sym];
   }
-  longestSeqContainedIn(a, b) {
-    for (var len = a.length; len >= 3; --len) {
-      for (var i = 0; i < a.length - len; ++i) {
-        for (var j = 0; j < b.length - len; ++j) {
-          let matches = true;
-          for (var k = 0; k < len; ++k) {
-            if (a[i + k] !== b[j + k]) {
-              matches = false;
-              break;
-            }
-          }
-          if (matches)
-            return [ len, i, j ];
-        }
-      }
-    }
-
-    return [ 0, 0, 0 ];
+  longestSeqCobtainedIn(ownStack, errStack) {
+    return longestSeqContainedIn(ownStack, errStack);
   }
-  enhanceStackTrace(err, own) { // Kinda copied the following and previous method :)
+  enhanceStackTrace(err, own) { // Kinda copied the following method from Node :)
     const sep = "\nEmitted 'error' event at:\n";
     const errStack = err.stack.split("\n").slice(1);
     const ownStack = own.stack.split("\n").slice(1);
-    const [len, off] = this.longestSeqContainedIn(ownStack, errStack);
+    const [len, off] = longestSeqContainedIn(ownStack, errStack);
     if (len > 0) ownStack.splice(off + 1, len - 1, '    [... lines matching original stack trace ...]');
     err.stack = err.stack + sep + ownStack.join("\n");
-    return err.stack;
   }
 }
 
