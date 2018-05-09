f154e00b9690f2970c12843de57f3ed7cba65882
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-05-09 10:09:21 +0100

    Update util.js

--- util.js
+++ util.js
@@ -19,8 +19,33 @@ class Util {
     if (!this.symbols[sym]) return sym;
     return this.symbols[sym];
   }
-  enhanceStackTrace(err, own) { // Heavily inspired by node itself
-    
+  longestSeqContainedIn(a, b) {
+    for (var len = a.length; len >= 3; --len) {
+      for (var i = 0; i < a.length - len; ++i) {
+        for (var j = 0; j < b.length - len; ++j) {
+          let matches = true;
+          for (var k = 0; k < len; ++k) {
+            if (a[i + k] !== b[j + k]) {
+              matches = false;
+              break;
+            }
+          }
+          if (matches)
+            return [ len, i, j ];
+        }
+      }
+    }
+
+    return [ 0, 0, 0 ];
+  }
+  enhanceStackTrace(err, own) { // Kinda copied the following and previous method :)
+    const sep = "\nEmitted 'error' event at:\n";
+    const errStack = err.stack.split("\n").slice(1);
+    const ownStack = own.stack.split("\n").slice(1);
+    const [len, off] = this.longestSeqContainedIn(ownStack, errStack);
+    if (len > 0) ownStack.splice(off + 1, len - 1, '    [... lines matching original stack trace ...]');
+    err.stack = err.stack + sep + ownStack.join("\n");
+    return err.stack;
   }
 }
 
